e5ded6ab4264bea2cd2cea3cc7bc67842ac6f713
Олег <31664872+Amos57@users.noreply.github.com>
2019-01-03 16:09:39 +0700

    Update Response.java
    Новый метод ,для получение JSON при взаимодействии с ajax

--- src/com/amos/server/web/Response.java
+++ src/com/amos/server/web/Response.java
@@ -1,29 +1,21 @@
 package com.amos.server.web;
 
 import java.io.PrintWriter;
-import java.nio.charset.Charset;
-
-import com.amos.server.annotation.HttpServet;
-import com.amos.server.servlets.Page;
 
 public interface Response {
 
-	
-	
-	
 	PrintWriter getWriter();
 	
-	void   addHeader(String name,Object value);
+	void addHeader(String name,Object value);
 	
 	void sendRedirect(String pathURL);
 	
-	void setEncoding(Charset charset);
+	void setEncoding(String charset);
 
-	int getContentLenght();
+	int  getContentLenght();
 
-	//temp
 	void send();
-
-	//void addServlet(HttpServet...httpServets);
+	
+	void sendJsonObject(JSONObject jsonObject);
 	
 }
