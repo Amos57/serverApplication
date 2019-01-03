ae55dedc92f1fb61877682b339d24709ad0590b7
Олег <31664872+Amos57@users.noreply.github.com>
2019-01-03 16:52:01 +0700

    Update Query.java

--- src/com/amos/server/despetcher/Query.java
+++ src/com/amos/server/despetcher/Query.java
@@ -6,11 +6,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
-import com.amos.server.annotation.HttpServet;
 import com.amos.server.web.HttpRequest;
 import com.amos.server.web.HttpRespons;
+import com.amos.server.web.HttpServlet;
 import com.amos.server.web.Request;
 import com.amos.server.web.Response;
 
@@ -18,48 +17,48 @@ public class Query {
 
 	private Request request;
 	private Response response;
-	private HttpServet servet;
-	private ServletLoader sl= ServletLoader.getInstance();
-	public Query(Socket socket) throws IOException {
-		
-		BufferedReader br= new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-		request= new HttpRequest(br);
-		response= new HttpRespons(bw);
-		
-	}
+	private HttpServlet servet;
+	private ServletLoader sl = ServletLoader.getInstance();
 
+	public Query(Socket socket) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		request = new HttpRequest(br);
+		response = new HttpRespons(bw);
+
+	}
 
 	public Request getRequest() {
 		return request;
 	}
 
-
 	public Response getResponse() {
 		return response;
 	}
 
-    private boolean goServlet(){
-    	servet=sl.getServlet(request.getURL());
-    	if(servet!=null){
-    		if(request.getMethod().equals("GET"))
-    			servet.doGet(request, response);
-    		else if(request.getMethod().equals("POST"))
-    			servet.doPost(request, response);
-    		return true;
-    	}
+	private boolean goServlet() {
+		servet = sl.getServlet(request.getURL());
+		if (servet != null) {
+			if (request.getMethod().equals("GET"))
+				servet.doGet(request, response);
+			else if (request.getMethod().equals("POST"))
+				servet.doPost(request, response);
+			return true;
+		}
 		return false;
-    	
-    }
 
-
-public void forward(){
-	if(!goServlet()){
-		response.setEncoding(StandardCharsets.UTF_8);
-		response.sendRedirect(request.getURL());
 	}
-	response.send();
-}
 
-	
+	public void forward() {
+		try {
+			if (!goServlet()) {
+				response.sendRedirect(request.getURL());
+			}
+			response.send();
+		} catch (Exception e) {System.out.println(e.getMessage()+" empty response");}
+			
+		
+	}
+
 }
